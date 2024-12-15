71e9785b7a62527c47eed9a3b1a43911206e0e30
MohanGanesh <144591373+mohanganesh3@users.noreply.github.com>
2024-12-15 13:24:07 +0530

    Update crew.py

--- src/newsletter/crew.py
+++ src/newsletter/crew.py
@@ -4,7 +4,6 @@ from newsletter.tools.research import SearchAndContents, FindSimilar, GetContent
 from langchain_anthropic import ChatAnthropic
 from langchain_groq import ChatGroq
 from datetime import datetime
-import streamlit as st
 from typing import Union, List, Tuple, Dict
 from langchain_core.agents import AgentFinish
 import json
@@ -27,44 +26,7 @@ class NewsletterGenCrew:
 
         return llm
 
-    def step_callback(
-        self,
-        agent_output: Union[str, List[Tuple[Dict, str]], AgentFinish],
-        agent_name,
-        *args,
-    ):
-        with st.chat_message("AI"):
-            # Try to parse the output if it is a JSON string
-            if isinstance(agent_output, str):
-                try:
-                    agent_output = json.loads(agent_output)
-                except json.JSONDecodeError:
-                    pass
-
-            if isinstance(agent_output, list) and all(
-                isinstance(item, tuple) for item in agent_output
-            ):
-
-                for action, description in agent_output:
-                    # Print attributes based on assumed structure
-                    st.write(f"Agent Name: {agent_name}")
-                    st.write(f"Tool used: {getattr(action, 'tool', 'Unknown')}")
-                    st.write(f"Tool input: {getattr(action, 'tool_input', 'Unknown')}")
-                    st.write(f"{getattr(action, 'log', 'Unknown')}")
-                    with st.expander("Show observation"):
-                        st.markdown(f"Observation\n\n{description}")
-
-            # Check if the output is a dictionary as in the second case
-            elif isinstance(agent_output, AgentFinish):
-                st.write(f"Agent Name: {agent_name}")
-                output = agent_output.return_values
-                st.write(f"I finished my task:\n{output['output']}")
-
-            # Handle unexpected formats
-            else:
-                st.write(type(agent_output))
-                st.write(agent_output)
-
+    
     @agent
     def researcher(self) -> Agent:
         return Agent(
